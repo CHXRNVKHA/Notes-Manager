d27aeef69734bfe2cc4c219edc732a28d84f193a
Dmitry <dzmitry.veka@itechart-group.com>
2022-11-21 02:39:57 +0300

    feat: add note component

---
+++ src/app/components/note/note.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-note',
+    templateUrl: './note.component.html',
+    styleUrls: ['./note.component.scss']
+})
+
+export class NoteComponent {}
